3d7d086a068004b670fb200c385f870bf9e212f6
Idriss Mortabit <idriss@localhost.localdomain>
2019-02-25 14:12:02 -0500

    getbill fixed

--- innoscripta_test/api/getbills.py
+++ innoscripta_test/api/getbills.py
@@ -7,29 +7,29 @@ from rest_framework.response import Response
 # b.save()
 @api_view(['POST'])
 def GetBill(request):
-    My_t_rechnungstermine = t_rechnungstermine(
-      id
-      company
-      date
-      purpose
-      reminder
-      amount
-      billNumber
-      fraction
-      done
-      status
-      billHtml
-      dueDate
-      Komment
-      halfbills
-      printeddate
-      signeddate
-      Unsicher
-      ignored
-      aktenziech
-      billtext
-      payday
-      csvfilename
-    )
-    My_t_rechnungstermine.save()
+    # My_t_rechnungstermine = t_rechnungstermine(
+    #   id
+    #   company
+    #   date
+    #   purpose
+    #   reminder
+    #   amount
+    #   billNumber
+    #   fraction
+    #   done
+    #   status
+    #   billHtml
+    #   dueDate
+    #   Komment
+    #   halfbills
+    #   printeddate
+    #   signeddate
+    #   Unsicher
+    #   ignored
+    #   aktenziech
+    #   billtext
+    #   payday
+    #   csvfilename
+    # )
+    # My_t_rechnungstermine.save()
   return Response(data)
