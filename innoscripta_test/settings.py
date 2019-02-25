6a9efd9fd86b77f47af64a226b8bd1787fdc28c4
Idriss Mortabit <idriss.mortabit@extentia.com>
2019-02-25 19:30:27 +0530

    Deploying

--- innoscripta_test/settings.py
+++ innoscripta_test/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'data.apps.MyappConfig',
+    'data.apps.DataConfig',
     'rest_framework',
-    'data',
 ]
 
 MIDDLEWARE = [
