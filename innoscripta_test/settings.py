57c3487ed2ba2d636b5ca7fc2f075fddac713e5d
Idriss Mortabit <idriss.mortabit@extentia.com>
2019-02-26 13:20:18 +0530

    settings update

--- innoscripta_test/settings.py
+++ innoscripta_test/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 static_dir = BASE_DIR + '/static/'
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-) 
+STATICFILES_DIRS = [static_dir]
+STATIC_ROOT = os.path.join(BASE_DIR, 'styles/')
+STATIC_ROOT = os.path.join(BASE_DIR, '*/')
